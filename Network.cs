2298eccacf2b3255b1065c9482ca6f1f4aec6936
joahangf11 <joahangarciaf@hotmail.com>
2023-03-30 12:03:59 -0600

    Se agrego boton Recibir JSON

--- Network.cs
+++ Network.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -15,6 +16,71 @@ public class Network : MonoBehaviour
     public TMP_InputField ifNombre;
     public TMP_InputField ifPuntos;
 
+    //La estructura que se convierte a JSON
+    public struct DatosUsuario
+    {
+        public string nombre;
+        public int puntos;
+    }
+
+    //Datos que se convierten a JSON para poder ir al servidor
+    public DatosUsuario datos;
+
+    //Click del botón Leer JSON
+    public void LeerJSON()
+    {
+        StartCoroutine(DescargarJSON());
+    }
+
+    IEnumerator DescargarJSON()
+    {
+        UnityWebRequest request = UnityWebRequest.Get("http://localhost/unity/enviaJSON.php");
+        yield return request.SendWebRequest();
+        //Después de cierto tiempo
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            //Descarga Exitosa
+            string textoJson = request.downloadHandler.text;
+            resultado.text = textoJson;
+
+            DatosUsuario datos = JsonUtility.FromJson<DatosUsuario>(textoJson);
+            resultado.text = resultado.text + "\n\n" + datos.nombre + "-" + datos.puntos;
+        }
+        else
+        {
+            resultado.text = "Error en la descarga: " + request.responseCode.ToString();
+        }
+        request.Dispose();
+    }
+
+
+    //Click del botón Subir JSON
+    public void EscribirJSON()
+    {
+        StartCoroutine(SubirJSON());
+    }
+
+    IEnumerator SubirJSON()
+    {
+        datos.nombre = ifNombre.text;
+        datos.puntos = Int32.Parse(ifPuntos.text);
+
+        WWWForm forma = new WWWForm();
+        forma.AddField("datosJSON", JsonUtility.ToJson(datos));
+
+        UnityWebRequest request = UnityWebRequest.Post("http://localhost/unity/recibeJSON.php", forma);
+        yield return request.SendWebRequest();
+        //Después de cierto tiempo
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            //Descarga Exitosa
+            resultado.text = request.downloadHandler.text;
+        }
+        else
+        {
+            resultado.text = "Error en la descarga: " + request.responseCode.ToString();
+        }
+    }
 
     //Click del botón Enviar Texto
     public void EnviarTextoPlano()
